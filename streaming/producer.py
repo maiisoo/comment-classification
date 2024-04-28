a205db156aa91ef963916dbfb1a821ba48e08585
quangnm-sonat <quangnm@sonat.vn>
2024-04-28 10:08:57 +0700

    fix user_id

--- streaming/producer.py
+++ streaming/producer.py
@@ -1,9 +1,9 @@
 import json
 import random
 import time
+import pandas
 from kafka import KafkaProducer
 from datetime import datetime
-
 def json_serializer(data):
     return json.dumps(data).encode("utf-8")
 
@@ -12,13 +12,13 @@ producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=json_serializer)
 
 #user_id,platform, timestamp, text, post_id,topic, label
-
+df = pandas.read_csv('dataset\\users.csv')
 click_info = []
 topics = ['politics', 'sports'];
 platforms = ['instagram', 'facebook', 'twitter', 'linkedin']
 texts= ['a love b', 'a love c', 'a love d', 'a love e', 'a love f', 'a love g', 'a love h', 'a love i', 'a love j', 'a love k', 'a love l']
 def random_info():
-    user_id = str(random.randint(100, 999)) + '-' + str(random.randint(1000, 9999)) + '-' + str(random.randint(1000, 9999))
+    user_id = random.choice(df['User_id'].tolist())
     platform = platforms[random.randint(0, 3)]
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     post_id = str(random.randint(1, 9999999999))
